5a5f0e840c1ec4346f402b2cd4a5baa2e90a84f4
Axe-P <axepax72@gmail.com>
2024-11-29 04:29:07 -0700

    more testing

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -15,7 +15,7 @@ interface DecodedToken {
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
   // Get the token from Authorization header, query parameters, or body
-  let token = req.headers.authorization || req.query.token || req.body.token;
+  let token = req.headers.authorization?.split(' ')[1] || req.query.token || req.body.token;
   console.log("Received token:", token);  // Log to see where the token comes from
 
   // Check if token is provided
