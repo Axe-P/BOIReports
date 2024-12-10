d6a4724b3eb16d55fcb19731c7d6a8eaef33b6a8
Axe-P <axepax72@gmail.com>
2024-12-09 23:28:25 -0700

    test

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -10,18 +10,12 @@ const HomePage = () => {
 
   return (
     <div className="home-page">
-      <h1>Welcome to Pax Accounting!</h1>
-      <p className="boir-description">
-        The Beneficial Ownership Information Report (BOIR) is a new requirement under the Corporate Transparency Act for U.S. businesses.
-      </p>
-      <ul className="boir-info">
-        <li>- Applies to most U.S. businesses</li>
-        <li>- Deadline for filing is January 1st, 2025</li>
-        <li>- Failure to file can result in penalties of up to $592 per day</li>
-      </ul>
-      <p className="cta-text">Click the button below to start filling out your report:</p>
+      <h3>File your BOI Report with peace of mind</h3>
+      <h1>Pax Accounting</h1>
+      <p>We will file your form within 48 hours of submission</p>
+      <p><strong>Pricing: $125</strong></p>
       <button className="start-filing-btn" onClick={handleNavigateToForm}>
-        Start Filing Now!
+        Click here to file now!
       </button>
     </div>
   );
